8e2c41a6bc9474e367e3502926ecd8b020d4a58e
jdjin3000 <57910677+jdjin3000@users.noreply.github.com>
2019-12-02 02:45:03 +0900

    마이페이지 이벤트 완성

--- WebContent/IconEvent.js
+++ WebContent/IconEvent.js
@@ -5,13 +5,13 @@ var followState=0;
 
 $(function(){
 	$('.follow-status').click(function(){
-		
-	      if(followState==0){
+		  var temp = "rgb(255, 69, 0)";
+	      if($(this).css("color") == temp){
 	        $(this).css("background","orangered");
 	        $(this).css("color","white");
-	        $(this).css("font-weight","bold");
+	        $(this).css("font-weight","bol d");
 	        $(this).css("transition","300ms ease all");
-	        followState=1;
+	        
 	      }
 	      else{
 		    $(this).css("background","rgb(250,250,250)");
@@ -19,14 +19,14 @@ $(function(){
 		    $(this).css("font-weight","300");
 		    $(this).css("color","orangered");
 		    $(this).css("transition","300ms ease all");
-	        followState=0;
-	      }
+	     }
 	  });
 });
 
 $(function(){
   $('.fa-heart').click(function(){
-      if(likeState==0){
+    var temp2 = "rgb(0, 0, 0)";
+      if($(this).css("color") == temp2){
         $(this).css("color","rgb(255, 78, 78)");
         $(this).css("font-weight","900");
         $(this).css("transition","500ms ease all");
@@ -46,13 +46,14 @@ $(function(){
 {/* <script type="text/javascript"> */}
 $(function(){
   $('.fa-bookmark').click(function(){
-      if(bookMarkState==0){
+    var temp3 = "400";
+      if($(this).css("font-weight") == temp3){
         $(this).css("font-weight","900");
         $(this).css("transition","300ms ease all");
         bookMarkState=1;
       }
       else{
-        $(this).css("font-weight","100");
+        $(this).css("font-weight","400");
         $(this).css("transition","350ms ease all");
         bookMarkState=0;
       }
